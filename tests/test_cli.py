6f51fa03becfa8eee1134070e2d9e1730f9b1642
Luke Plant <L.Plant.98@cantab.net>
2022-10-24 12:29:41 +0100

    Test rewrite

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -25,15 +25,7 @@ else:
             os.chdir(self._old_cwd.pop())
 
 
-def assert_stdout_contains(capsys, args, text):
-    assert_stdout(capsys, args, lambda output: text in output)
-
-
-def assert_stdout_does_not_contain(capsys, args, text):
-    assert_stdout(capsys, args, lambda output: text not in output)
-
-
-def assert_stdout(capsys, args, func):
+def assert_stdout(capsys, args, contains=None, does_not_contain=None):
     try:
         with chdir(os.path.dirname(__file__) + "/examples/test_cli"):
             main(args)
@@ -41,17 +33,19 @@ def assert_stdout(capsys, args, func):
         pass
     output = capsys.readouterr().out
     with capsys.disabled():
-        assert func(output)
+        if contains is not None:
+            assert contains in output
+        if does_not_contain is not None:
+            assert does_not_contain not in output
 
 
 @pytest.mark.parametrize("arg", ["-h", "--help"])
 def test_help(capsys, arg):
-    assert_stdout_contains(capsys, [arg], "Grep Python files")
+    assert_stdout(capsys, [arg], contains="Grep Python files")
 
 
 def test_search(capsys):
     # Negative test to ensure we aren't accidentally grepping this test code,
     # which could happen if CWD is wrong.
-    assert_stdout_does_not_contain(capsys, [".//*"], "Not real code")
-
-    assert_stdout_contains(capsys, [".//Name"], "./misc.py:3:12:    return an_arg")
+    assert_stdout(capsys, [".//*"], does_not_contain="Not real code")
+    assert_stdout(capsys, [".//Name"], contains="./misc.py:3:12:    return an_arg")
